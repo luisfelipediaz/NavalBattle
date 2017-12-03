21fbfac2183753b0ab1f0f9d7a38996af8e2e207
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-03 17:50:49 -0500

    Separate responsibility of add player to new class named "NavalBattleGame"

--- Source/Codifico.Senior.Core/Entities/NavalBattleGames.cs
+++ Source/Codifico.Senior.Core/Entities/NavalBattleGames.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Codifico.Senior.Core.Entities
 {
     public class NavalBattleGames
     {
-        
+        public List<NavalBattleGame> Games { get; set; }
+
+        public NavalBattleGame AddPlayer(string Id)
+        {
+            NavalBattleGame groupAlone = Games.FirstOrDefault(game => game.Player2 is null);
+
+            if (groupAlone.Player1 is null)
+            {
+                groupAlone.Player1 = new Player { Id = Id };
+                Games.Add(groupAlone);
+            }
+            else
+            {
+                groupAlone.Player2 = new Player { Id = Id };
+            }
+
+            return groupAlone;
+        }
     }
 }
