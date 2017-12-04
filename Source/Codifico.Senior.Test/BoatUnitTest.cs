74c7c48e7890f5016c4cbc0a7a5c9d804bad70fb
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-03 20:44:57 -0500

    Tests of Boat

---
+++ Source/Codifico.Senior.Test/BoatUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Codifico.Senior.Core.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Codifico.Senior.Test
+{
+    [TestClass]
+    public class BoatUnitTest
+    {
+        [TestMethod]
+        public void ShouldDisabledBoatWhenAllPointsAreBeaten()
+        {
+            Boat boat = new Boat(new Point(1, 2), new Point(1, 3));
+
+            Point move = new Point(1, 2);
+            boat.HitMarker(move);
+
+            Point move2 = new Point(1, 3);
+            boat.HitMarker(move2);
+
+            Assert.IsFalse(boat.ItsAlive());
+        }
+
+
+        [TestMethod]
+        public void ShouldHitMarkerReturnTrueWhenHitMarkerAssert()
+        {
+            Boat boat = new Boat(new Point(1, 2), new Point(1, 4));
+            Boolean beaten = boat.HitMarker(new Point(1, 3));
+            Assert.IsTrue(beaten);
+        }
+
+        [TestMethod]
+        public void ShouldHitMarkerReturnFalseWhenHitMarkerNoAssert()
+        {
+            Boat boat = new Boat(new Point(1, 2), new Point(1, 4));
+            Boolean beaten = boat.HitMarker(new Point(4, 2));
+            Assert.IsFalse(beaten);
+        }
+
+
+        [TestMethod]
+        public void ShouldItsAliveReturnTrueWhenAllPointsAreBeaten()
+        {
+            Boat boat = new Boat(new Point(1, 2), new Point(1, 3));
+            boat.HitMarker(new Point(1, 2));
+
+            Assert.IsTrue(boat.ItsAlive());
+        }
+
+        [TestMethod]
+        public void ShouldItsAliveReturnFalseWhenAllPointsAreBeaten()
+        {
+            Boat boat = new Boat(new Point(1, 2), new Point(1, 2));
+            boat.HitMarker(new Point(1, 2));
+
+            Assert.IsFalse(boat.ItsAlive());
+        }
+    }
+}
