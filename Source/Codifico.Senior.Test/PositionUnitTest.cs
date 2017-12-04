bfac05c0c3ff96b71ed7a1237c3631734ed752cd
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-03 20:43:39 -0500

    Tests of the class named utilitary Position

---
+++ Source/Codifico.Senior.Test/PositionUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Codifico.Senior.Core.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codifico.Senior.Test
+{
+    [TestClass]
+    public class PositionUnitTest
+    {
+        [TestMethod]
+        public void ShouldCreateAPositionWith3Points()
+        {
+            var position = Position.InitPointsNotMarked(new Point(1, 2), new Point(1, 4));
+
+            Assert.AreEqual(3, position.Count);
+        }
+
+
+    }
+}
