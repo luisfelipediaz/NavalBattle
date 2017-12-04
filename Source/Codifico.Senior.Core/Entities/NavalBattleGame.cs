d2ab8dbb57199c603ac91f17f4d68c188758bba6
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-04 09:54:47 -0500

    Encapsule player 1 and 2 for have control on create boats per player

--- Source/Codifico.Senior.Core/Entities/NavalBattleGame.cs
+++ Source/Codifico.Senior.Core/Entities/NavalBattleGame.cs
@@ -5,9 +5,9 @@ namespace Codifico.Senior.Core.Entities
     {
         public string Id { get; }
 
-        public Player Player1 { get; set; }
+        Player Player1 { get; set; }
 
-        public Player Player2 { get; set; }
+        Player Player2 { get; set; }
 
         public Player Winner { get; set; }
 
@@ -29,6 +29,42 @@ namespace Codifico.Senior.Core.Entities
             }
 
             throw new ArgumentException("Player not exist");
+        }
+
+        public void AssignPlayer1(Player player)
+        {
+            if (Player1 is null)
+            {
+                Player1 = player;
+                ProcessPlayerBoats(Player1);
+            }
+            else
+            {
+                throw new System.ArgumentException("The player 1 already exists");
+            }
+        }
+
+        public void AssignPlayer2(Player player)
+        {
+            if (Player2 is null)
+            {
+                Player2 = player;
+                ProcessPlayerBoats(Player2);
+            }
+            else
+            {
+                throw new System.ArgumentException("The player 1 already exists");
+            }
+        }
+
+        private void ProcessPlayerBoats(Player player)
+        {
+            //TODO
+        }
+
+        public bool Player2Missing()
+        {
+            return Player2 is null;
         }
     }
 }
--- Source/Codifico.Senior.Test/NavalBattleGameTestUnit.cs
+++ Source/Codifico.Senior.Test/NavalBattleGameTestUnit.cs
@@ -17,11 +17,9 @@ namespace Codifico.Senior.Test
         public void ShouldReturnThePlayer1WhenSendPlayerAny()
         {
             Player expected = new Player("PlayerAny");
-            NavalBattleGame game = new NavalBattleGame
-            {
-                Player1 = new Player("PlayerAny"),
-                Player2 = new Player("Player1")
-            };
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("PlayerAny"));
+            game.AssignPlayer2(new Player("Player1"));
 
             Player actualPlayer = game.GetPlayer("PlayerAny");
 
@@ -32,11 +30,9 @@ namespace Codifico.Senior.Test
         public void ShouldReturnThePlayer2WhenSendPlayerAny()
         {
             Player expected = new Player("PlayerAny");
-            NavalBattleGame game = new NavalBattleGame
-            {
-                Player1 = new Player("Player1"),
-                Player2 = new Player("PlayerAny")
-            };
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("Player1"));
+            game.AssignPlayer2(new Player("PlayerAny"));
 
             Player actualPlayer = game.GetPlayer("PlayerAny");
 
@@ -47,12 +43,47 @@ namespace Codifico.Senior.Test
         [ExpectedException(typeof(System.ArgumentException))]
         public void ShouldThrowExceptionWhenSendPlayerError()
         {
-            NavalBattleGame game = new NavalBattleGame
-            {
-                Player1 = new Player("Player1"),
-                Player2 = new Player("PlayerAny")
-            };
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("Player1"));
+            game.AssignPlayer2(new Player("PlayerAny"));
             game.GetPlayer("PlayerError");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ShouldPlayer2MissingReturnTrueWhilePlayer2IsNotDefined()
+        {
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("Player1"));
+            Assert.IsTrue(game.Player2Missing());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ShouldPlayer2MissingReturnTrueWhenPlayer2IsDefined()
+        {
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("Player1"));
+            game.AssignPlayer2(new Player("Player2"));
+            Assert.IsFalse(game.Player2Missing());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ShouldNotPermiteOverridePlayer1()
+        {
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer1(new Player("Player1"));
+            game.AssignPlayer1(new Player("PlayerAny"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ShouldNotPermiteOverridePlayer2()
+        {
+            NavalBattleGame game = new NavalBattleGame();
+            game.AssignPlayer2(new Player("Player2"));
+            game.AssignPlayer2(new Player("PlayerAny"));
         }
     }
 }
