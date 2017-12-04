db6fe3adc9a53117a016d91988f2963225d93016
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-03 20:44:50 -0500

    Tests of class NavalBattleGames

---
+++ Source/Codifico.Senior.Test/NavalBattleGamesTestUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Codifico.Senior.Core.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codifico.Senior.Test
+{
+    [TestClass]
+    public class NavalBattleGamesTestUnit
+    {
+        [TestMethod]
+        public void ShouldReturnNavalBattleGameOnAddPlayerOneAndTwo()
+        {
+            NavalBattleGames games = new NavalBattleGames();
+            NavalBattleGame gamePlayer1 = games.AddPlayer("player1");
+            NavalBattleGame gamePlayer2 = games.AddPlayer("player2");
+
+            Assert.IsFalse(gamePlayer1 is null);
+            Assert.IsFalse(gamePlayer2 is null);
+
+            Assert.AreEqual(gamePlayer1, gamePlayer2);
+        }
+
+        [TestMethod]
+        public void ShouldCreatedNewNavaBattleOfExistsTheyAreFull()
+        {
+            NavalBattleGames games = new NavalBattleGames();
+            games.AddPlayer("player1");
+            NavalBattleGame gamePlayer2 = games.AddPlayer("player2");
+            NavalBattleGame gamePlayer3 = games.AddPlayer("player3");
+            Assert.AreNotEqual(gamePlayer2, gamePlayer3);
+        }
+    }
+}
