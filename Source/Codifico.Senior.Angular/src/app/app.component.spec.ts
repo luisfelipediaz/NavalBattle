ad5159c968db13b64b90d2db12c3dced237cc752
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-04 23:28:19 -0500

    Unit test module

--- Source/Codifico.Senior.Angular/src/app/app.component.spec.ts
+++ Source/Codifico.Senior.Angular/src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavalBattleComponent } from './naval-battle/naval-battle.component';
+import { OpposingBoardComponent } from './opposing-board/opposing-board.component';
+import { OwnBoardComponent } from './own-board/own-board.component';
+import { NavalBattleService } from './services/naval-battle.service';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,24 +12,19 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavalBattleComponent,
+        OpposingBoardComponent,
+        OwnBoardComponent
       ],
+      providers: [
+        NavalBattleService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- Source/Codifico.Senior.Angular/src/app/app.module.ts
+++ Source/Codifico.Senior.Angular/src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavalBattleComponent } from './naval-battle/naval-battle.component';
 import { NavalBattleService } from './services/naval-battle.service';
+import { OpposingBoardComponent } from './opposing-board/opposing-board.component';
+import { OwnBoardComponent } from './own-board/own-board.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavalBattleComponent
+    NavalBattleComponent,
+    OpposingBoardComponent,
+    OwnBoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [NavalBattleService],
   bootstrap: [AppComponent]
