bee816698dd57400fdfc76e32432435cc51bc5d9
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2017-12-04 23:24:21 -0500

    Add subject startGame, for the init game notification client and render board

--- Source/Codifico.Senior.Angular/src/app/services/naval-battle.service.ts
+++ Source/Codifico.Senior.Angular/src/app/services/naval-battle.service.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { HubConnection } from '@aspnet/signalr-client';
+import { environment } from '../../environments/environment';
+import { NavalBattleGame } from '../app.model';
 
 @Injectable()
 export class NavalBattleService {
 
   private movesOfRival: Subject<any> = new Subject<any>();
+  private startGame: Subject<NavalBattleGame> = new Subject<NavalBattleGame>();
   private hubConnection: HubConnection;
 
   constructor() {
-    this.initSignalR();
+    
   }
 
-  private initSignalR() {
-    this.hubConnection = new HubConnection('http://localhost:5000/navalBattle');
+  public initGame(): Observable<NavalBattleGame> {
+    this.hubConnection = new HubConnection(environment.urlNavalSignalR);
     this.createHubs();
     this.startSignalR();
+    return this.startGame;
   }
 
   private startSignalR() {
@@ -30,11 +34,11 @@ export class NavalBattleService {
       this.movesOfRival.next(data);
     });
 
-    this.hubConnection.on('onAssignGame', (groupName: string) => {
-      alert(groupName);
+    this.hubConnection.on('onAssignGame', (game: NavalBattleGame) => {
+      this.startGame.next(game);
     });
   }
-
+ 
   onMoveRival(): Observable<any> {
     return this.movesOfRival;
   }
